cf7893146f6c1fb04838fcc9e927097fe50c51a2
yuxin.wang <yuxin_wang_94@163.com>
2024-03-20 14:48:37 +0800

    UI (#53)
    * Update dependencies and add new models
    * Update models and fix image format bug
    
    ---------
    
    Co-authored-by: wangyuxin <wangyuxin@mokahr.com>

--- generate/platforms/zhipu.py
+++ generate/platforms/zhipu.py
@@ -16,7 +16,7 @@ class ZhipuSettings(PlatformSettings):
 
     api_key: SecretStr
     v3_api_base: str = 'https://open.bigmodel.cn/api/paas/v3/model-api'
-    v4_api_base: str = 'https://open.bigmodel.cn/api/paas/v4/'
+    v4_api_base: str = 'https://open.bigmodel.cn/api/paas/v4'
     platform_url: str = 'https://open.bigmodel.cn/dev/howuse/introduction'
 
 
