49998d2c81739c9e25d3cc9cd04cd794da182d09
liuze <liuze@mokahr.com>
2024-06-11 20:19:10 +0800

    add support moonshot function call

--- generate/chat_completion/models/moonshot.py
+++ generate/chat_completion/models/moonshot.py
@@ -11,13 +11,23 @@ from generate.chat_completion.models.openai_like import OpenAILikeChat
 from generate.http import HttpClient
 from generate.model import ModelParameters, RemoteModelParametersDict
 from generate.platforms import MoonshotSettings
-from generate.types import Probability, Temperature
-
+from generate.types import Probability, Temperature, OrIterable
+from generate.chat_completion.tool import FunctionJsonSchema, Tool, ToolCallMixin
+from generate.chat_completion.models.openai_like import (
+    FunctionCallName,
+    OpenAILikeChat,
+    OpenAIResponseFormat,
+    OpenAITool,
+    OpenAIToolChoice,
+    convert_to_openai_tool,
+)
+from generate.utils import ensure_iterable
 
 class MoonshotChatParameters(ModelParameters):
     temperature: Optional[Temperature] = None
     top_p: Optional[Probability] = None
     max_tokens: Optional[PositiveInt] = None
+    tools: Optional[List[OpenAITool]] = None
 
 
 class MoonshotParametersDict(RemoteModelParametersDict, total=False):
@@ -26,7 +36,7 @@ class MoonshotParametersDict(RemoteModelParametersDict, total=False):
     max_tokens: PositiveInt
 
 
-class MoonshotChat(OpenAILikeChat):
+class MoonshotChat(OpenAILikeChat, ToolCallMixin):
     model_type: ClassVar[str] = 'moonshot'
     available_models: ClassVar[List[str]] = ['moonshot-v1-8k', 'moonshot-v1-32k', 'moonshot-v1-128k']
 
@@ -64,3 +74,12 @@ class MoonshotChat(OpenAILikeChat):
     ) -> AsyncIterator[ChatCompletionStreamOutput]:
         async for stream_output in super().async_stream_generate(prompt, **kwargs):
             yield stream_output
+
+
+    @override
+    def add_tools(self, tools: OrIterable[Tool]) -> None:
+        new_tools = [convert_to_openai_tool(tool) for tool in ensure_iterable(tools)]
+        if self.parameters.tools is None:
+            self.parameters.tools = new_tools
+        else:
+            self.parameters.tools.extend(new_tools)
